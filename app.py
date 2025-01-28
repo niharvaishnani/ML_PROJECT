8cb9b923fe288e14b1b25b69aa1c1348468e3fbe
niharvaishnani <167516198+niharvaishnani@users.noreply.github.com>
2025-01-28 20:53:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,9 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
 
 # 0.irrelevant
 # 1.Negative 
