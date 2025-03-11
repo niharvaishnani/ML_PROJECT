abf94278b71b8b4a507ad328c8334cb4b3a31944
niharvaishnani <167516198+niharvaishnani@users.noreply.github.com>
2025-03-11 11:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,10 @@
-# from flask import Flask, request, jsonify
-# #from flask_cors import CORS
-# import joblib
-# import pandas as pd
-# from sklearn.preprocessing import LabelEncoder
-
-
-# app = Flask(__name__)
-
-# # Load your trained model
-# model = joblib.load('ipl.pkl')
-# #CORS(app)  # Enable CORS for all routes
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.json  # Get JSON data sent in the POST request
-    
-#     df = pd.DataFrame([data])  
-    
-#     demo2 = {}
-#     for column in ['batting_team', 'bowling_team', 'city']:
-#         le = LabelEncoder()
-#         df[column] = le.fit_transform(df[column])
-#         demo2[column] = le  # Save the encoder for future use
-
-#     prediction = model.predict(df)
-
-#     return jsonify({'prediction': prediction.tolist()}) 
-
-# #if __name__ == '__main__':
-#     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import os 
+import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the trained model and vectorizer
 model_path = "uploads/model.pkl"  # Replace with the actual path to your model
@@ -107,9 +51,3 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
-
-
-# 0.irrelevant
-# 1.Negative 
-# 2.Netural
-# 3.Positive
